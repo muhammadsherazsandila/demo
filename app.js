4b787c73be4b570d4a308ec5ff3405eec4d46c69
muhammad sheraz <msd.sheraz046@gmail.com>
2024-12-05 18:50:51 +0500

    adding views

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => res.send("Express on Vercel"));
+const userModel = mongoose.model("user" , userSchema)
+app.get("/", async (req, res) =>{
+    res.render("home")
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
