f1c4ade69a9dbe9874d219fe4bfd4d29c648b7b4
muhammad sheraz <msd.sheraz046@gmail.com>
2024-12-06 17:30:14 +0500

    okay

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.set("view egine" , "ejs")
+app.set("view engine" , "ejs")
 app.get("/", async (req, res) =>{
     res.render("home")
 });
